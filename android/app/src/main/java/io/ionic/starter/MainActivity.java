18ef67f5e070e371b4a638f87030decf87261f4c
USER <kommado7899@gmail.com>
2023-08-01 10:43:54 +0700

    Edit Barcode

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -6,12 +6,12 @@ import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
-import com.dutchconcepts.capacitor.barcodescanner.BarcodeScanner;
+// import com.dutchconcepts.capacitor.barcodescanner.BarcodeScanner;
 
 public class MainActivity extends BridgeActivity {
     @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    registerPlugin(BarcodeScanner.class);
+    // registerPlugin(BarcodeScanner.class);
   }
 }
